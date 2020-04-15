0ef45d61adeec34d8371f81e58eb3f2da28134fb
Aditya Ganti <aganti@sumologic.com>
2020-04-15 14:11:41 +0530

    Fix tests

--- meqa/mqgo/main_test.go
+++ meqa/mqgo/main_test.go
@@ -9,20 +9,18 @@ import (
 )
 
 func TestMqgo(t *testing.T) {
+	var baseURL, username, password, apitoken string
 	wd, _ := os.Getwd()
 	meqaPath := filepath.Join(wd, "../../testdata")
 	swaggerPath := filepath.Join(meqaPath, "petstore_meqa.yml")
 	planPath := filepath.Join(meqaPath, "simple.yml")
 	resultPath := filepath.Join(meqaPath, "result.yml")
 	testToRun := "all"
-	baseURL := ""
-	username := ""
-	password := ""
-	apitoken := ""
+	fuzzType := 0
 	verbose := false
 
 	mqutil.Logger = mqutil.NewFileLogger(filepath.Join(meqaPath, "mqgo.log"))
-	runMeqa(&meqaPath, &swaggerPath, &planPath, &resultPath, &testToRun, &username, &password, &apitoken, &baseURL, &verbose)
+	runMeqa(&meqaPath, &swaggerPath, &planPath, &resultPath, &testToRun, &username, &password, &apitoken, &baseURL, &fuzzType, &verbose)
 }
 
 func TestMain(m *testing.M) {
